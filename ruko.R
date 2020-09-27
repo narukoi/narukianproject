1cde4353c63154d92a5477889bb51e01d63d6c5e
Ian Bogley <ibogley@uoregon.edu>
2020-09-26 17:16:11 -0700

    made progress on the carrier function

--- ruko.R
+++ ruko.R
@@ -8,4 +8,16 @@ rownames(board) <- LETTERS[seq.int(1,10)]
 carrier <- function(){
   orientation <- unlist(sample(c('h','v'),1))
   start_change <- unlist(sample(seq.int(1,10),1))
+  change_pos <- c(seq.int(start_change,start_change + 4))
+  
+  for (i in seq(1,5))  {if (change_pos[i]>10) {
+    change_pos[i] <- change_pos[i] - 10
+  }}
+  
+  ifelse(
+    orientation == 'h',
+    ,
+    )
+  return(change_pos)
 }
+carrier()
