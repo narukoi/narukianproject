4358762b7d4f9467e47f6dfb5658c5cddc60fb99
Ian Bogley <ibogley@uoregon.edu>
2020-09-29 08:49:48 -0700

    worked on battleship function

--- ruko.R
+++ ruko.R
@@ -47,3 +47,52 @@ carrier <- carrier_fun()
 #board 1/5: 1 board adding each ship progressively
 board_1_5 <- board
 board_1_5[unique(carrier$rows),unique(carrier$columns)] <- "C"
+
+
+
+### BATTLESHIP FUNCTION
+battleship_fun <- function(carrier_df) {
+  seed <- sample(seq.int(1,1000))
+  set.seed(seed)
+  #orientation determines the direction of a ship's placement
+  orientation <- unlist(sample(c('horizontal','vertical'),1))
+  
+  
+  #change_pos will be a vector of 5 numbers designating a ship's position along one axis
+  #if orientation is horizontal, change_pos = columns
+  #if orientation is vertical, change_pos = rows
+  start_change <- unlist(sample(seq.int(1,10),1))
+  change_pos <- c(seq.int(start_change,start_change + 3))
+  
+  
+  #we will also ensure that no part of the ship goes off the board
+  #if a number in change_pos is over 10, we subtract 5 from that element
+  for (i in seq(1,4))  {if (change_pos[i]>10) {
+    change_pos[i] <- change_pos[i] - 4
+  }}
+  
+  
+  #static_pos will be a single number denoting a ship's position along the other axis
+  static_pos <- unlist(sample(seq.int(1,10),1))
+  
+  
+  #now we will determine the final dataset for the carrier's position
+  if (orientation == 'horizontal') {
+    battleship_positions <- data.frame(
+      columns = change_pos,
+      rows = static_pos
+    )}
+  else {
+    battleship_positions <- data.frame(
+      columns = static_pos,
+      rows = change_pos
+    )}
+  
+  return(battleship_positions)
+  
+}
+battleship_fun()
+
+if () {
+  
+}
